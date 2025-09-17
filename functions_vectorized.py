2be396443240fa244c98aad8bfe09f2fdd854715
ydhyoxly <152093412+ydhyoxly@users.noreply.github.com>
2025-09-17 20:15:24 +0300

    Update functions_vectorized.py

--- functions_vectorized.py
+++ functions_vectorized.py
@@ -41,9 +41,10 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-img = Image.open("photo_2025-09-17 18.53.08.png")
+img = Image.open("изображение")
 
 img_arr = np.asarray(img)
+img_arr_1 = img_arr.tolist()
 
 
 
@@ -52,11 +53,7 @@ def vector_summ(image, weights):
     return np.tensordot(image, weights, axes=([2], [0]))
 
 
-
-
-
 gray_weights = np.array([0.299, 0.587, 0.114])
-
 
 plt.figure(figsize = (15, 10))
 
@@ -67,12 +64,14 @@ plt.title('Изначальное изображение')
 plt.axis('off')
 
 
-
 plt.subplot(1, 3, 3)
-gray_image_2 = vector_summ(synthetic_image, gray_weights)
-plt.imshow(gray_image_2, cmap='gray')
+gray_img_2 = vector_summ(img_arr, gray_weights)
+plt.imshow(gray_img_2, cmap='gray')
 plt.title('Векторизованное изображение')
 plt.axis('off')
+
+
+
 
 
 
